138825e73f03a3e9f2e59f7b3b1fda5d1fc5bf79
xinliHuaweiLaptop <sun-xl18@tsinghua.org.cn>
2025-08-13 10:52:45 +0800

    improve the github workflows, and hw_vol run.sh

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,7 +1,7 @@
 # Add your commands here
 docker build -t hw_vol_image:1.0 .
 
-docker run --rm --mount type=bind,source=$(pwd)/../src,target=/app hw_vol_image:1.0 /bin/bash -c "g++ -o main.out add.cpp"
+docker run --rm --mount type=bind,source=$(pwd)/../src,target=/app hw_vol_image:1.0 /bin/bash -c "g++ -o mul.out mul.cpp && ./mul.out 996 666"
 
 # NOTE: The value of `source` should be the absolute path of `../src` directory
 
